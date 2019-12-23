e866736fbd71045c9d9dee17fc44e793d627a572
SirJoraH21 <mr.vip.yurra90@gmail.com>
2019-12-24 00:00:59 +0200

    rename eslint file

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -2,14 +2,14 @@
     let b = parseInt(prompt('Enter second number'));
     let c = parseInt(prompt('Enter third number'));
     if (typeof (a, b, c) === 'number') {
-        d = Math.sqrt(Math.pow(b, 2) - (4 * a * c));
+       let d = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
         if (d > 0) {
-            x1 = (-b + d) / (2 * a);
-            x2 = (-b - d) / (2 * a);
+            let x1 = (-b + d) / (2 * a);
+            let x2 = (-b - d) / (2 * a);
             console.log('x1 = ' + x1 + ' and x2 = ' + x2);
-        } else if (d == 0) {
-            x = -b / (2 * a);
-            console.log("x = " + x)
+        } else if (d === 0) {
+            let x = -b / (2 * a);
+            console.log('x = ' + x)
         } else {
             console.log('no solution')
         }
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -5,10 +5,10 @@ if (isNaN(a) || isNaN(b) || isNaN(c)) {
     alert('input values should be ONLY numbers')
 } else {
     if (a > 0 && b > 0 && c > 0) {
-        if ((a + b) > c && (a + c) > b && (b + c) > a) {
-            if (a == b && b == c && a == c) {
+        if (a + b > c && a + c > b && b + c > a) {
+            if (a === b && b === c && a === c) {
                 console.log('Equilateral triangle')
-            } else if (a == b || b == c || a == c) {
+            } else if (a === b || b === c || a === c) {
                 console.log('Isosceles triangle')
             } else {
                 console.log('Scalene triangle')
